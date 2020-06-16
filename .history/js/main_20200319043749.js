675efb8fabb9178fd43c452890d3d16dee736cfa
expertsumyua <expert.sumy.ua@gmail.com>
2020-06-16 14:53:43 +0300

    добавил бургер!

--- .history/js/main_20200319043749.js
+++ .history/js/main_20200319043749.js
@@ -1,42 +1,3 @@
-var burger = document.querySelector("header .mobile img");
-var menu = document.querySelector("header nav");
-// let menuFloat = document.querySelector("header nav a");
-var burgerShow = false;
-var menuShow = true;
-var body = document.querySelector("body");
-burger.onclick = function () {
-
-	if (menu.style.display == "none" || menu.style.display == "") {
-		// if (!menuShow) {
-
-		// menuShow = true;
-		menu.style.margin = "50px";
-		menu.style.display = "block";
-	}
-	else {
-		// menuShow = false;
-		menu.style.display = "none";
-		menu.style.margin = "0px";
-	}
-}
-
-window.onresize = function (event) {
-	// if (menu.style.display == "none" || menu.style.display == "") { menuShow = false; }
-	// else { menuShow = true; }
-
-	if (body.offsetWidth <= 650) {
-		burgerShow = true;
-		menu.style.margin = "50px";
-	} else {
-		burgerShow = false;
-		menu.style.margin = "0px";
-	}
-
-	if (body.offsetWidth > 650 && (!burgerShow)) { menu.style.display = "block"; }
-	else { menu.style.display = "none"; }
-}
-
-
 /*
 1. Сверстать слайдер - done
 2. Сделать переключание фотографий влево в право
@@ -67,9 +28,9 @@ $("#main-slider img")
 
 // Клик по ПРАВОЙ стрелке
 $("#main-slider .next").click(function () {
-
-	if (currentImage < images.length - 1) {
-		currentImage++;
+	
+	if(currentImage < images.length - 1) {
+		currentImage ++;
 	} else {
 		currentImage = 0;
 	}
@@ -78,7 +39,7 @@ $("#main-slider .next").click(function () {
 		.attr("src", path + images[currentImage]);
 
 	// находим li по data-id
-	var $item = $('#slides ul').children('[data-id="' + currentImage + '"]');
+	var $item = $('#slides ul').children('[data-id="'+ currentImage +'"]');
 	// Убираем у всех эдементов класс 'active'
 	$("#slides ul li").removeClass('active');
 	// на єлементе который выбрали по data-id добавляем класс active
@@ -89,8 +50,8 @@ $("#main-slider .next").click(function () {
 // Клик по ЛЕВОЙ стрелке
 $("#main-slider .pref").click(function () {
 
-	if (currentImage > 0) {
-		currentImage--;
+	if(currentImage > 0 ) {
+		currentImage --;		
 	} else {
 		currentImage = images.length - 1;
 	}
@@ -99,7 +60,7 @@ $("#main-slider .pref").click(function () {
 		.attr("src", path + images[currentImage]);
 
 	// находим li по data-id
-	var $item = $('#slides ul').children('[data-id="' + currentImage + '"]');
+	var $item = $('#slides ul').children('[data-id="'+ currentImage +'"]');
 	// Убираем у всех эдементов класс 'active'
 	$("#slides ul li").removeClass('active');
 	// на єлементе который выбрали по data-id добавляем класс active
@@ -108,37 +69,37 @@ $("#main-slider .pref").click(function () {
 // Создане катрочек фотографий
 for (var i = 0; i < images.length; i++) {
 	// Добавляем элемаент в блок с миникартинками
-	$('#slides ul').append("<li data-id='" + i + "'><img src='"
+	$('#slides ul').append("<li data-id='" + i + "'><img src='" 
 		+ path + images[i] + "'></li>");
 	// Если этопервая капртинка добавляем класс 'active'
-	if (i == 0) {
+	if (i ==0) {
 		$('#slides ul li').addClass('active');
 	}
 }
 
 // Делаем Клик по слайдам
-$('#slides ul li').click(function (e) {
+$('#slides ul li').click(function(e) {
 	// Убираем у всех эдементов класс 'active'
 	$("#slides ul li").removeClass('active');
 	// на єлементе по которому кликнули добавляем класс active
 	$(this).addClass('active');
 	// получаем id элемента по которому кликнули
 	var id = e.currentTarget.dataset.id;
-
+	
 	// меняем картинку в слайде
 	$("#main-slider img")
 		.attr("src", path + images[id]);
 });
 
-$("#main-slider img").click(function () {
+$("#main-slider img").click(function(){
 	// console.log(this);
-	$('#opacity').css({ 'display': 'block' });
+	$('#opacity').css({'display':'block'});
 	$('#full-image')
-		.css({ 'display': 'block' })
-		.append('<img src="' + $(this).attr('src') + '">');
+	.css({'display':'block'})
+	.append('<img src="' + $(this).attr('src') + '">');
 });
 
-$('#opacity').click(function () {
-	$('#opacity').css({ 'display': 'none' });
-	$('#full-image').css({ 'display': 'none' }).empty();
+$('#opacity').click(function(){
+	$('#opacity').css({'display':'none'});
+	$('#full-image').css({'display':'none'}).empty();
 });
